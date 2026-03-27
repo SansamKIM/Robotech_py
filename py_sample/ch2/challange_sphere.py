a1060825cdd16bb8c35f22dc62a6e9c7d1892c9d
SansamKim <dm34941@gmail.com>
2026-03-27 13:57:34 +0900

    ch2 수정

--- py_sample/ch2/challange_sphere.py
+++ py_sample/ch2/challange_sphere.py
@@ -5,11 +5,13 @@
 pi = 3.141592
 r = int(input("구의 반지름을 입력하세요. >>> "))
 
-volum = (3 / 4) * pi * (r ** 3)
+volume = (3 / 4) * pi * (r ** 3)
 
 s_area = 4 * pi * r * r
 
-print("반지름{}의 부피는 {:.2f}입니다.".format(r, volum))
+print("반지름{}의 부피는 {:.2f}입니다.".format(r, volume))
 print("반지름{}의 겉넓이는 {:.2f}입니다.".format(r, s_area))
 
 # fstring 표현
+print(f"반지름{r}의 부피는 {volume:.2f}입니다.")
+print(f"반지름{r}의 겉넓이는 {s_area:.2f}입니다.")
