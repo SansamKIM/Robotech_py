165f58f0b8c21fceeda15bf9db5bec8a504191b4
SansamKim <dm34941@gmail.com>
2026-04-02 14:25:04 +0900

    5장 함수

---
+++ py_sample/ch5/ch5-1/param_basic.py
@@ -0,0 +1,7 @@
+# 매개변수의 기본
+
+def print_n_times(value, n):
+    for i in range(n):
+        print(value)
+        
+print_n_times("안녕하세요", 5)  # 매개변수가 맞지 않을 때 에러가 납니다.
---
+++ py_sample/ch5/ch5-1/return_only.py
@@ -0,0 +1,7 @@
+# 자료 없이 리턴하기 
+def return_test():
+    print("A의 위치입니다.")
+    return
+    print("B의 위치입니다.")
+
+return_test()    
