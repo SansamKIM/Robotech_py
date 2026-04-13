039a2e7670154e209b58dbdb88f94b59220d4b56
SansamKim <dm34941@gmail.com>
2026-04-13 12:22:11 +0900

    챕터8-2

--- py_sample/ch8/ch8-1/class_in_program.py
+++ py_sample/ch8/ch8-1/class_in_program.py
@@ -1,3 +1,6 @@
 class Game:
-    def __init__ (self, name, id, passwd, email, ...):
-    self.name = name
+    def __init__ (self, name, id, passwd, email):
+        self.name = name
+        self.id = id
+        self.passwd = passwd
+        self.email = email
